aeefc571f7f8b926930a695c101614bb53d55362
mirekondro1 <130686217+mirekondro@users.noreply.github.com>
2024-05-21 09:04:32 +0200

    creating and editing login navbar

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import Nav from "@/components/nav/Nav";
+import NavLogin from "@/components/nav-login/Nav-login";
 import SessionWrapper from "@/components/SessionWrapper";
 
 const inter = Inter({ subsets: ["latin"] });
@@ -20,7 +21,7 @@ export default function RootLayout({
     <SessionWrapper>
       <html lang="en">
         <body className={inter.className}>
-          <Nav/>
+          <NavLogin/>
           {children}
         </body>
       </html>
