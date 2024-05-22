6ae8c932cfde223e037e2d2b9b143cd929c352bc
Honza Běhunčík <honzabehuncik@gmail.com>
2024-05-22 07:56:55 +0200

    Added react-spring lib, fadein/out hero

--- src/components/hero/Hero.tsx
+++ src/components/hero/Hero.tsx
@@ -1,14 +1,38 @@
+"use client";
+
+import React, { useState, useEffect } from "react";
+import { useTransition, animated } from "@react-spring/web";
 import Link from "next/link";
 import "./hero.css";
 import { IoSearch } from "react-icons/io5";
 
 export default function Hero() {
+    const [currentTextIndex, setCurrentTextIndex] = useState(0);
+    const texts = ["Jídlo bez stresu a nepořádku!", "Rychlé a chutné doručení!", "Objevte nové chutě!", "Dneska nechte vaření na nás!", "Jídlo na dosah jedním klikem!", "Máte hlad? My víme, že ano!"];
+
+    useEffect(() => {
+        const interval = setInterval(() => {
+            setCurrentTextIndex((prevIndex) => (prevIndex + 1) % texts.length);
+        }, 10000);
+
+        return () => clearInterval(interval);
+    }, [texts.length]);
+
+    const transitions = useTransition(texts[currentTextIndex], {
+        from: { opacity: 0, transform: "translateY(-20px)" },
+        enter: { opacity: 1, transform: "translateY(0)" },
+        leave: { opacity: 0, transform: "translateY(20px)" },
+        config: { duration: 600 },
+    });
+
     return (
         <div>
             <div className="container">
                 <section className="landing">
-                    <h1>Jídlo bez stresu a nepořádku!</h1>
-                    <img className="aboutus-img" src="/landing-img.png"/>
+                    {transitions((style, item) => (
+                        <animated.h1 style={style}>{item}</animated.h1>
+                    ))}
+                    <img className="aboutus-img" src="/landing-img.png" />
                     <div className="location-box">
                         <div className="location-input">
                             <input type="text" id="adress-input" name="myInput" placeholder="Zadejte adresu pro doručení..."></input>
