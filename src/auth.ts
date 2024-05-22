dcff8ac3837724fa54c9e123468ccf6d5731a71c
Michal Hrabal <75084936+Hraby@users.noreply.github.com>
2024-05-22 08:03:40 +0200

    db: prisma eddge runtime

--- src/auth.ts
+++ src/auth.ts
@@ -1,19 +1,15 @@
 import NextAuth from "next-auth"
 import Google from "next-auth/providers/google"
 import { PrismaAdapter } from "@auth/prisma-adapter";
-import { PrismaClient } from "@prisma/client"
- 
-const prisma = new PrismaClient()
- 
+import prisma from "./lib/prisma";
+
+
 export const { handlers, signIn, signOut, auth } = NextAuth({
     adapter: PrismaAdapter(prisma),
     providers: [
         Google({
             clientId: process.env.GOOGLE_CLIENT_ID,
             clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-            profile(profile) {
-                return { role: profile.role ?? "user" };
-                }
             }),
     ],
     callbacks: {
