2c3cd79086d08fbf4d2f186968dd546f64e17577
Michal Hrabal <75084936+Hraby@users.noreply.github.com>
2024-05-28 09:50:49 +0200

    fix: navbars

--- src/components/nav/menu-nav.tsx
+++ src/components/nav/menu-nav.tsx
@@ -7,22 +7,12 @@ import { getOrders } from "@/lib/db";
 
 
 export default async function DashboardNav(){
-    const session = await auth()
-    let orders = null
-    let order = null
-    if(session?.user?.id){
-        orders = await getOrders(session?.user?.id)
-        if(orders && orders.length > 0){
-            order = orders[0].orderItems 
-        }
-    }
-
     return (
         <header>
             <nav>
                 <Link href="/" className="logo">GastroGO</Link>
                 <Search/>
-                <BasketMenu order={order}/>
+                <BasketMenu/>
             </nav>
         </header>
     );
